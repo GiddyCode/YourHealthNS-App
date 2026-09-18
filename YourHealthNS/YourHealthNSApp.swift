bf0f77f13fded7feb56054870a41e908fe6bf20f
Gideon Ogbonna <ogbonnagideon1994@gmail.com>
2026-09-18 09:11:14 -0600

    Initial Commit

---
+++ YourHealthNS/YourHealthNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourHealthNSApp.swift
+//  YourHealthNS
+//
+//  Created by Gideon Ogbonna on 2026-09-18.
+//
+
+import SwiftUI
+
+@main
+struct YourHealthNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
